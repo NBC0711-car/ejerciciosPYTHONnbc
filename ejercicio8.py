692dbf299bf47ab15fc84fc0b0e1f3f21afdc2eb
Nicolas Baya-Casal Sansolini <nbaysan0711@ieszaidinvergeles.org>
2025-10-07 13:16:46 +0200

    ejercio 8 hecho

---
+++ ejercicio8.py
@@ -0,0 +1,5 @@
+n = int(input('Introduce el dividendo (entero n): '))
+m = int(input('Introduce el divisor (entero m): '))
+cociente = n // m
+resto = n % m
+print(f'{n} entre {m} da un cociente {cociente} y un resto {resto}')
