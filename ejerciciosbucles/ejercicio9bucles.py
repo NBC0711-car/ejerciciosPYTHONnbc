3d07a06466d4b8e2e0a60b4d705d70a4669e11bf
Nicolas Baya-Casal Sansolini <nbaysan0711@ieszaidinvergeles.org>
2025-11-05 10:05:04 +0100

    ejercicio 9 de bucles hecho

---
+++ ejerciciosbucles/ejercicio9bucles.py
@@ -0,0 +1,9 @@
+contraseña = input('sabes la contraseña? ')
+
+clave = 'nomelase'
+
+while contraseña != clave:
+    print('incorrecta')
+    contraseña = input('vuelve a intentarlo: ')
+
+print('pase usted')
