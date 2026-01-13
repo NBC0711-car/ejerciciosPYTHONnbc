d31491585826c1fb05db58d7ce8884fe8ddf9810
Nicolas Baya-Casal Sansolini <nbaysan0711@ieszaidinvergeles.org>
2026-01-13 13:41:47 +0100

    modified exercise 8 so it uses lists

--- ejercicios listas/ejercicio8listas.py
+++ ejercicios listas/ejercicio8listas.py
@@ -1,6 +1,18 @@
 palabra = input("Introduce una palabra: ")
-palabra_reves = palabra[::-1]
-if palabra == palabra_reves:
+letras = list(palabra)
+
+es_palindromo = True
+n = len(letras)
+
+for i in range(n // 2):
+    primera = letras[i]
+    ultima = letras[n - 1 - i]
+    
+    if primera != ultima:
+        es_palindromo = False
+        break
+
+if es_palindromo:
     print("Es un palíndromo")
 else:
     print("No es un palíndromo")
