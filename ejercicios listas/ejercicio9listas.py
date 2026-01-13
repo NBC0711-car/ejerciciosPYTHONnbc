20a003bc0dc031dd9a55397a68a0bab7abfb7ca5
Nicolas Baya-Casal Sansolini <nbaysan0711@ieszaidinvergeles.org>
2026-01-13 13:45:05 +0100

    exercise 9 from lists done

---
+++ ejercicios listas/ejercicio9listas.py
@@ -0,0 +1,8 @@
+palabra = input("Introduce una palabra: ")
+vocales = ["a", "e", "i", "o", "u"]
+for vocal in vocales:
+    contador = 0
+    for letra in palabra:
+        if letra == vocal:
+            contador += 1
+    print("La vocal " + vocal + " aparece " + str(contador) + " veces")
